cfc6518796aa727ab31bc5015c5497a7b96ca212
Mostafa <gguuhhidj@gmail.com>
2024-08-14 21:13:58 +0300

    Product Card ans Page

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -5,14 +5,9 @@ import ProductMainCard from "../Cards/ProductMainCard";
 const Products = () => {
     return (
         <>
-            <div class="container">
+            <div class="container" style={{ padding: "100px 0" }}>
                 <div class="row">
-                    <ProductMainCard />
-                    <ProductMainCard />
-                    <ProductMainCard />
-                    <ProductMainCard />
-                    <ProductMainCard />
-                    <ProductMainCard />
+                    <ProductMainCard></ProductMainCard>
                 </div>
             </div>
         </>
