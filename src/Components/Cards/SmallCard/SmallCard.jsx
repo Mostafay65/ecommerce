1c2604aaddeed37043f7974e4dadb39665d473a3
Abdo Elbessa <zezoelbessa10@gmail.com>
2024-08-15 14:55:45 +0300

    Your message

---
+++ src/Components/Cards/SmallCard/SmallCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SCard1 from "./../../../imgs/theme/SmallCard/feature-1.png";
+
+const SmallCard = () => {
+  return (
+    <>
+      <div className="  container mt-5 justify-content-between align-content-center">
+       <div className="row">
+       <div className="col-lg-2 col-md-4 mb-md-3 mb-lg-0 bg-lamon pe-1 ">
+          <img src={SCard1} alt="" />
+          <h5>Free Shipping</h5>
+        </div>
+        <div className="col-lg-2 col-md-4 mb-md-3 mb-lg-0 bg-lamon pe-1 ">
+          <img src={SCard1} alt="" />
+          <h5>Free Shipping</h5>
+        </div>
+        <div className="col-lg-2 col-md-4 mb-md-3 mb-lg-0 bg-lamon pe-1 ">
+          <img src={SCard1} alt="" />
+          <h5>Free Shipping</h5>
+        </div>
+        <div className="col-lg-2 col-md-4 mb-md-3 mb-lg-0 bg-lamon pe-1 ">
+          <img src={SCard1} alt="" />
+          <h5>Free Shipping</h5>
+        </div>
+        <div className="col-lg-2 col-md-4 mb-md-3 mb-lg-0 bg-lamon pe-1 ">
+          <img src={SCard1} alt="" />
+          <h5>Free Shipping</h5>
+        </div>
+        <div className="col-lg-2 col-md-4 mb-md-3 mb-lg-0 bg-lamon pe-1 ">
+          <img src={SCard1} alt="" />
+          <h5>Free Shipping</h5>
+        </div>
+       </div>
+      </div>
+    </>
+  );
+};
+
+export default SmallCard;
