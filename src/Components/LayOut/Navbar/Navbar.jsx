eaafc9d7a4227c4cc7f9fd56fcca0baf9aa266e9
muhamadd9 <mhmdramadan.192@gmail.com>
2024-08-18 06:56:33 +0300

    handle clicking Links in navBar

--- src/Components/LayOut/Navbar/Navbar.jsx
+++ src/Components/LayOut/Navbar/Navbar.jsx
@@ -12,11 +12,13 @@ const Navbar = () => {
   const location = useLocation();
   let { numOfItems, getCart } = useContext(CartContext);
   let userName = localStorage.getItem("userName");
+
   function handleSignOut() {
     localStorage.removeItem("userToken");
     localStorage.removeItem("userName");
     setToken(null);
   }
+
   async function getUserCart() {
     if (token) await getCart();
   }
@@ -40,6 +42,16 @@ const Navbar = () => {
     getUserCart();
   }, [token]);
 
+  const handleLinkClick = () => {
+    
+    const navbarToggler = document.querySelector('.navbar-collapse');
+    if (navbarToggler) {
+      navbarToggler.classList.remove('show');
+    }
+    window.scrollTo(0, 0);
+    setScroll(false)
+  };
+
   return (
     <>
       <nav
@@ -49,8 +61,7 @@ const Navbar = () => {
       >
         <div className="container px-2">
           <div className="logo me-3">
-            <Link to={"/home"}>
-              {" "}
+            <Link to={"/home"} onClick={handleLinkClick}>
               <img src={logo} style={{ width: "120px" }} alt="Logo" />
             </Link>
           </div>
@@ -70,90 +81,98 @@ const Navbar = () => {
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {(token == "admin" || localStorage.getItem('userName') == 'admin') ?<>
-                <NavLink
-                  to="/allusers"
-                  className={({ isActive }) =>
-                    `${
-                      isActive || location.pathname === "/allusers" ? "text-main" : ""
-                    } nav-link`
-                  }
-                  aria-current="page"
-                >
-                  All Users
-                </NavLink>
-                <NavLink
-                  to="/messages"
-                  className={({ isActive }) =>
-                    `${
-                      isActive || location.pathname === "/messages" ? "text-main" : ""
-                    } nav-link`
-                  }
-                  aria-current="page"
-                >
-                  Messages
-                </NavLink>
-              
-              </>:
-              <>
-              <li className="nav-item">
-                <NavLink
-                  to="/home"
-                  className={({ isActive }) =>
-                    `${
-                      isActive || location.pathname === "/" ? "text-main" : ""
-                    } nav-link`
-                  }
-                  aria-current="page"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/products"
-                  className={({ isActive }) =>
-                    `${isActive ? "text-main" : ""} nav-link`
-                  }
-                  aria-current="page"
-                >
-                  Products
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/about-us"
-                  className={({ isActive }) =>
-                    `${isActive ? "text-main" : ""} nav-link`
-                  }
-                  aria-current="page"
-                >
-                  About Us
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/contact-us"
-                  className={({ isActive }) =>
-                    `${isActive ? "text-main" : ""} nav-link`
-                  }
-                  aria-current="page"
-                >
-                  Contact 
-                </NavLink>
-              </li>
-              </>}
+              {(token === "admin" || localStorage.getItem('userName') === 'admin') ? (
+                <>
+                  <NavLink
+                    to="/allusers"
+                    className={({ isActive }) =>
+                      `${
+                        isActive || location.pathname === "/allusers" ? "text-main" : ""
+                      } nav-link`
+                    }
+                    aria-current="page"
+                    onClick={handleLinkClick}
+                  >
+                    All Users
+                  </NavLink>
+                  <NavLink
+                    to="/messages"
+                    className={({ isActive }) =>
+                      `${
+                        isActive || location.pathname === "/messages" ? "text-main" : ""
+                      } nav-link`
+                    }
+                    aria-current="page"
+                    onClick={handleLinkClick}
+                  >
+                    Messages
+                  </NavLink>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/home"
+                      className={({ isActive }) =>
+                        `${
+                          isActive || location.pathname === "/" ? "text-main" : ""
+                        } nav-link`
+                      }
+                      aria-current="page"
+                      onClick={handleLinkClick}
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/products"
+                      className={({ isActive }) =>
+                        `${isActive ? "text-main" : ""} nav-link`
+                      }
+                      aria-current="page"
+                      onClick={handleLinkClick}
+                    >
+                      Products
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/about-us"
+                      className={({ isActive }) =>
+                        `${isActive ? "text-main" : ""} nav-link`
+                      }
+                      aria-current="page"
+                      onClick={handleLinkClick}
+                    >
+                      About Us
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/contact-us"
+                      className={({ isActive }) =>
+                        `${isActive ? "text-main" : ""} nav-link`
+                      }
+                      aria-current="page"
+                      onClick={handleLinkClick}
+                    >
+                      Contact 
+                    </NavLink>
+                  </li>
+                </>
+              )}
             </ul>
 
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               {(token || localStorage.getItem('userName')) ? (
                 <>
-                 <div className=" d-flex me-0 me-lg-3 mt-2 mb-3 my-lg-0 justify-content-center align-items-center">
-                   <Link to={'/cart'}>
-                   <i className="fas fa-xl fa-cart-shopping text-sec position-relative">
-                      <div className="bg-main position-absolute bottom-100 start-100 translate-middle text-white text-xsm d-flex justify-content-center align-items-center rounded-3 " style={{width:'18px',height:'18px'}}>{numOfItems}</div>
-                    </i>
-                   </Link>
+                  <div className=" d-flex me-0 me-lg-3 mt-2 mb-3 my-lg-0 justify-content-center align-items-center">
+                    <Link to={'/cart'} onClick={handleLinkClick}>
+                      <i className="fas fa-xl fa-cart-shopping text-sec position-relative">
+                        <div className="bg-main position-absolute bottom-100 start-100 translate-middle text-white text-xsm d-flex justify-content-center align-items-center rounded-3 " style={{width:'18px',height:'18px'}}>{numOfItems}</div>
+                      </i>
+                    </Link>
                   </div>
                   <div className="user-info justify-content-center d-flex align-items-center">
                     <div
@@ -178,6 +197,7 @@ const Navbar = () => {
                           to="/changePassword"
                           className={`text-main mb-1  ${styles.logoutIcon} text-decoration-none`}
                           aria-current="page"
+                          onClick={handleLinkClick}
                         >
                           <p className="my-0 ">Change Password</p>
                         </NavLink>
@@ -186,14 +206,16 @@ const Navbar = () => {
                           to="/"
                           className={`text-main d-block mb-1 text-decoration-none ${styles.logoutIcon} `}
                           aria-current="page"
-                          onClick={handleSignOut}
+                          onClick={() => {
+                            handleSignOut();
+                            handleLinkClick();
+                          }}
                         >
                           Log Out
                         </NavLink>
                       </div>
                     </div>
                   </div>
-                 
                 </>
               ) : (
                 <>
@@ -204,6 +226,7 @@ const Navbar = () => {
                         `${isActive ? "text-main" : ""} nav-link`
                       }
                       aria-current="page"
+                      onClick={handleLinkClick}
                     >
                       Login
                     </NavLink>
@@ -215,6 +238,7 @@ const Navbar = () => {
                         `${isActive ? "text-main" : ""} nav-link`
                       }
                       aria-current="page"
+                      onClick={handleLinkClick}
                     >
                       Register
                     </NavLink>
